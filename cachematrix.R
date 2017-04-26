0f3189d38164e47cfa8088af4f74d53c5339a8e0
Atlen <yratnikov@gmail.com>
2017-04-26 09:31:59 +0300

    code added with somecomments
    code added with somecomments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## It is a coursera R programming assignment No.2 
 
-## Write a short comment describing this function
+## makeCachematrix is setting the value of the matrix, getting the value, 
+## making inversion of the matrix while setting its value into the environment.
+## and it is also getting the inverted matrix from environment as a result
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                inv <- NULL
+                  set <- function(y) {
+                                       x <<- y
+                                        inv <<- NULL
+                     }
+          get <- function() x
+          setinverse <- function(inverse) inv <<- inverse
+          getinverse <- function() inv
+          list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+##Next function below is
+##searching of the previously stored value of invertible matrix within the environment
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+              inv <- x$getinverse()
+              if(!is.null(inv)) {
+               message("getting cached data.") ## message appears if the value was stored before.
+               return(inv)
+               }
+        data <- x$get()
+        inv <- solve(data)
+        x$setinverse(inv)
+        inv
 }
+
+##End of code
